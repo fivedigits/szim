5c3e7d93d78465b1162bc4b000d502cd4132728c
Moritz Schönherr <moritz.schoenherr@gmail.com>
2016-05-04 11:52:39 +0200

    corrected typos in name, removed how to apply section

--- szim.sh
+++ szim.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 #SZiM is a UNIX citation manager written in BASH.
-#    Copyright (C) 2016  Moritz Schönherrr
+#    Copyright (C) 2016  Moritz Schönherr
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
